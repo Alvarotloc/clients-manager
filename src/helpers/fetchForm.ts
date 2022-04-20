d215e775a1dc721a6f86fe3762949677ea8d376a
Alvarotloc <alvarotloc@gmail.com>
2022-04-20 17:55:50 +0200

    Opción de delete lista, hecho todo el crud

--- src/helpers/fetchForm.ts
+++ src/helpers/fetchForm.ts
@@ -1,11 +1,16 @@
 import { valoresForm } from '../components/Formulario';
 
-export const fetchForm = (url:string,method:string,body:valoresForm) => {
+export const fetchForm = (url:string,method:string,body?:valoresForm) => {
+    if(body){
+        return fetch(url, {
+            method: method,
+            body: JSON.stringify(body),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }).then((res) => res.json());
+    }
     fetch(url, {
-        method: method,
-        body: JSON.stringify(body),
-        headers: {
-          "Content-Type": "application/json",
-        },
+        method: method
       }).then((res) => res.json());
 }
--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { ClientesType } from '../types/clientes';
 import Cliente from '../components/Cliente';
+import { fetchForm } from '../helpers/fetchForm';
 
 const Inicio = ():JSX.Element => {
     const [clientes, setClientes] = useState<ClientesType[]>([]);
@@ -12,11 +13,26 @@ const Inicio = ():JSX.Element => {
             const resultado = await respuesta.json();
             setClientes(resultado);
         } catch (error) {
-            
+            console.log(error)
         }
        }
        obtenerClientes(); 
     },[])
+
+    const handleEliminar = async(id:number) => {
+        const confirmar = confirm('¿Deseas eliminar este cliente?');
+        if(confirmar){
+            try {
+                const url = `http://localhost:4000/clientes/${id}`;
+                fetchForm(url,'DELETE');
+
+                const arrayClientes = clientes.filter(cliente => cliente.id !== id);
+                setClientes(arrayClientes);
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    }
   return (
     <>
     <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
@@ -33,7 +49,7 @@ const Inicio = ():JSX.Element => {
         </thead>
         <tbody>
             {clientes.map(cliente => (
-                <Cliente key={cliente.id} cliente={cliente}/>
+                <Cliente key={cliente.id} cliente={cliente} handleEliminar={handleEliminar}/>
             ))}
         </tbody>
     </table>
